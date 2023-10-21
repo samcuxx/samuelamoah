3277ca1dccc13513ae4b5ff90b5c749623621ce1
lilyoo226 <samuellove228@gmail.com>
2023-10-21 08:58:58 +0000

    🥴fixed Heroes Section

--- app/components/pages/Heroes.tsx
+++ app/components/pages/Heroes.tsx
@@ -7,16 +7,16 @@ export default async function Heroes() {
   const heroes: HeroeType[] = await getHeroes();
 
   return (
-    <section className="mt-32 max-w-5xl">
+    <section className="max-w-5xl mt-32">
       <Slide delay={0.17}>
-        <h2 className="text-4xl mb-4 font-bold tracking-tight">Heroes</h2>
-        <p className="dark:text-zinc-400 text-zinc-600 max-w-2xl">
+        <h2 className="mb-4 text-4xl font-bold tracking-tight">Heroes</h2>
+        <p className="max-w-2xl dark:text-zinc-400 text-zinc-600">
           Inspired by{" "}
           <a
             href="https://rafa.design"
             rel="noreferrer noopener"
             target="_blank"
-            className="dark:text-blue-400 text-blue-500 underline"
+            className="text-blue-500 dark:text-blue-400"
           >
             Rafael Conde&apos;s
           </a>{" "}
@@ -28,11 +28,11 @@ export default async function Heroes() {
         </p>
       </Slide>
 
-      <ul className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6 mt-12 tracking-tight">
+      <ul className="grid grid-cols-1 gap-6 mt-12 tracking-tight lg:grid-cols-4 md:grid-cols-2">
         {heroes.map((heroe) => (
           <li
             key={heroe._id}
-            className="flex items-center gap-x-2 dark:bg-primary-bg bg-zinc-100 border dark:border-zinc-800 border-zinc-200 rounded-md px-2 py-1"
+            className="flex items-center px-2 py-1 border rounded-md gap-x-2 dark:bg-primary-bg bg-zinc-100 dark:border-zinc-800 border-zinc-200"
           >
             <EasterEgg isMet={heroe.met} />
             <a

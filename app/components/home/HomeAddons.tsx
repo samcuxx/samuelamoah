34d9181972e9a0c80adf4bfe22d20a9ed7abea5c
lilyoo226 <samuellove228@gmail.com>
2023-10-21 07:58:30 +0000

    💥Add more Bg color

--- app/components/home/HomeAddons.tsx
+++ app/components/home/HomeAddons.tsx
@@ -19,19 +19,20 @@ const HomeAddons = () => {
           <ProjectList />
         </Slide>
       </section>
-        <section className="mt-32">
-          <Slide delay={0.16}>
-            <div className="mb-16">
-              <h2 className="font-incognito text-4xl mb-4 font-bold tracking-tight">
-                My Featured Posts
-              </h2>
-            </div>
-          </Slide>
-          <Slide delay={0.18} className="flex gap-5">
-            <FeaturedPosts />
-          </Slide>
-          <Heroes/>
-        </section>
+      <section className="mt-32">
+        <Slide delay={0.16}>
+          <div className="mb-16">
+            <h2 className="font-incognito text-4xl mb-4 font-bold tracking-tight">
+              My Featured Posts
+            </h2>
+          </div>
+        </Slide>
+        <Slide delay={0.18} className="flex gap-5">
+          <FeaturedPosts />
+        </Slide>
+        <Heroes />
+        <div className="blurb-1 bottom-[-80%] left-[10%]"></div>
+      </section>
     </>
   );
 };

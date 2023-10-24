ea8e0b4010afb80f0c31ba283ccec0de585cd857
Samuel Amoah <94200301+lilyoo226@users.noreply.github.com>
2023-10-24 18:28:02 +0000

    adsencs fixed

--- app/page.tsx
+++ app/page.tsx
@@ -11,37 +11,34 @@ export default async function Home() {
   const profile: ProfileType[] = await getProfile();
 
   return (
-    <>
-      <main className="px-6 mx-auto mt-20 max-w-7xl md:px-16 lg:mt-32">
-        {/* <SVGContainer viewBox="0 0 800 600"/> */}
-        <section className="flex flex-col items-start justify-between mb-16 xl:flex-row xl:items-center xl:justify-center gap-x-12">
-          {profile &&
-            profile.map((data) => (
-              <div key={data._id} className="max-w-2xl lg:max-w-2xl">
-                <Slide>
-                  <h1 className="font-incognito font-black tracking-tight text-3xl sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full">
-                    {data.headline}
-                  </h1>
-                  <p className="text-base leading-relaxed dark:text-zinc-400 text-zinc-600">
-                    {data.shortBio}
-                  </p>
-                </Slide>
-                <Slide delay={0.1}>
-                  <Social type="social" />
-                </Slide>
-              </div>
-            ))}
-          <Slide delay={0.14}>
-            <HeroSvg />
-            {/* <SVGContainer viewBox="0 0 800 600"/> */}
-          </Slide>
-          <div className="blurb-1 bottom-[-20%] right-[10%]"></div>
-        </section>
-        <Job />
-        {/* <HomeAddons/> */}
-      </main>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5234293399921884"
-     crossOrigin="anonymous"></script>
-    </>
+    <main className="px-6 mx-auto mt-20 max-w-7xl md:px-16 lg:mt-32">
+      {/* <SVGContainer viewBox="0 0 800 600"/> */}
+      <section className="flex flex-col items-start justify-between mb-16 xl:flex-row xl:items-center xl:justify-center gap-x-12">
+        {profile &&
+          profile.map((data) => (
+            <div key={data._id} className="max-w-2xl lg:max-w-2xl">
+              <Slide>
+                <h1 className="font-incognito font-black tracking-tight text-3xl sm:text-5xl mb-6 lg:leading-[3.7rem] leading-tight lg:min-w-[700px] min-w-full">
+                  {data.headline}
+                </h1>
+                <p className="text-base leading-relaxed dark:text-zinc-400 text-zinc-600">
+                  {data.shortBio}
+                </p>
+              </Slide>
+              <Slide delay={0.1}>
+                <Social type="social" />
+              </Slide>
+            </div>
+          ))}
+        <Slide delay={0.14}>
+          <HeroSvg />
+      {/* <SVGContainer viewBox="0 0 800 600"/> */}
+
+        </Slide>
+        <div className="blurb-1 bottom-[-20%] right-[10%]"></div>
+      </section>
+      <Job />
+      {/* <HomeAddons/> */}
+    </main>
   );
 }
